09869ca1c32fc53149668ba4a547cbbdb12dfe2c
SujayBonde <sujaybonde2005@gmail.com>
2025-09-27 11:59:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <>
+    <ToastContainer />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/login" element={<Login />}>
+        </Route>
+        <Route path="/email-verify" element={<EmailVerify />}>
+        </Route>
+        <Route path="/reset-password" element={<ResetPassword />}>
+        </Route>
+      </Routes>
+    </div>
+    </>
+  );
+}
+
+export default App;
